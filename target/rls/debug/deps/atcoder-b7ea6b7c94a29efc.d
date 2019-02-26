11292bf5622bbc822aaf48c68e0b01dcc64d0b52
peacock <peacock0803sz@gmail.com>
2019-02-27 08:13:02 +0900

    :fire: Delete Rust files

--- target/rls/debug/deps/atcoder-b7ea6b7c94a29efc.d
+++
@@ -1,5 +0,0 @@
-e:\Users\yoichi\VirtualBox\ManjaroLinux\share\github.com\peacock0803sz\atcoder\target\rls\debug\deps\atcoder-b7ea6b7c94a29efc.rmeta: src\main.rs
-
-e:\Users\yoichi\VirtualBox\ManjaroLinux\share\github.com\peacock0803sz\atcoder\target\rls\debug\deps\atcoder-b7ea6b7c94a29efc.d: src\main.rs
-
-src\main.rs:
--- target/rls/debug/deps/atcoder-fd2f5c1bb9f59c72.d
+++
@@ -1,5 +0,0 @@
-e:\Users\yoichi\VirtualBox\ManjaroLinux\share\github.com\peacock0803sz\atcoder\target\rls\debug\deps\atcoder-fd2f5c1bb9f59c72.rmeta: src\main.rs
-
-e:\Users\yoichi\VirtualBox\ManjaroLinux\share\github.com\peacock0803sz\atcoder\target\rls\debug\deps\atcoder-fd2f5c1bb9f59c72.d: src\main.rs
-
-src\main.rs:
