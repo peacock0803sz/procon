72d7ebb0b25f9d7a672ad703ba9d41115592b8ea
peacock <peacock0803sz@gmail.com>
2019-02-27 18:51:54 +0900

    :zap: ABC083B
    Answer

--- beginners-selection/abc083b.py
+++ beginners-selection/abc083b.py
@@ -1,16 +1,6 @@
-n, a, b = int(input())
-
-
-def digits_sum(x):
-    sum = 0
-    if x > 0:
-        sum += x % 10
-        x /= 10
-    return sum
-
-
+n, a, b = map(int, input().split())
 answer = 0
-for i in (n + 1):
-    if a < digits_sum(n) < b:
+for i in range(n + 1):
+    if a <= sum(list(map(int, list(str(i))))) <= b:
         answer += 1
 print(answer)
