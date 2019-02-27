0e97ce5ee72b7c979eec694f783ab89b26ee7ef4
peacock <peacock0803sz@gmail.com>
2019-02-27 09:07:07 +0900

    :art: ABC081A

---
+++ beginners-selection/abc081a.py
@@ -0,0 +1,17 @@
+s = int(input())
+if s == 0:
+    print(0)
+elif s == 100:
+    print(1)
+elif s == 10:
+    print(1)
+elif s == 1:
+    print(1)
+elif s == 110:
+    print(2)
+elif s == 101:
+    print(2)
+elif s == 11:
+    print(2)
+elif s == 111:
+    print(3)
