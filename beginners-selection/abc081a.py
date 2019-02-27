e55f1d46f7609a81043c1f050141b14da30a18b3
peacock <peacock0803sz@gmail.com>
2019-02-27 09:12:42 +0900

    :zap: ABC081A
    count()

--- beginners-selection/abc081a.py
+++ beginners-selection/abc081a.py
@@ -1,17 +1,2 @@
-s = int(input())
-if s == 0:
-    print(0)
-elif s == 100:
-    print(1)
-elif s == 10:
-    print(1)
-elif s == 1:
-    print(1)
-elif s == 110:
-    print(2)
-elif s == 101:
-    print(2)
-elif s == 11:
-    print(2)
-elif s == 111:
-    print(3)
+s = input()
+print(s.count('1'))
