93fc51ee57f57ccd119b7c51d4a553c7658724a9
peacock <peacock0803sz@gmail.com>
2019-03-02 20:44:13 +0900

    :zap: ABC085C

--- beginners-selection/8_abc085c.py
+++ beginners-selection/8_abc085c.py
@@ -1,15 +1,9 @@
-N, Y = map(int, input().split())
-x = 0
-y = 0
-z = 0
-if Y % 10000 == 0:
-    x = Y / 10000
-else:
-    if Y % 5000 == 0:
-        y = Y / 5000
-    else:
-        z = Y / 1000
-if x + y + z <= N:
-    print(x, y, z)
-else:
-    print('-1 -1 -1')
+import sys
+
+n, yen = map(int, input().split())
+for x in range(n + 1):
+    for y in range(n - x + 1):
+        if x * 10000 + y * 5000 + (n - x - y) * 1000 == yen:
+            print(x, y, n - x - y)
+            sys.exit()
+print('-1 -1 -1')
