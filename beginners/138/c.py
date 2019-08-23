f86c2dbec8efeff9d66e5bb6a1d48e2b81895db3
peacock <peacock0803sz@gmail.com>
2019-08-24 02:56:01 +0900

    :sparkles: Pass task C

--- beginners/138/c.py
+++ beginners/138/c.py
@@ -1,12 +1,7 @@
-import math
-import itertools
-
 n = int(input())
 v = [int(i) for i in input().split()]
-ans = []
-comb = list(itertools.combinations(v, 2))
-for i in range(len(comb)):
-    list(comb[i])
-    ans.append((comb[i][0] + comb[i][1]) / 2)
-    print((comb[i][0] + comb[i][1]) / 2)
-print(max(ans))
+v.sort()
+ans = v[0]
+for i in range(1, len(v)):
+    ans = (ans + v[i]) / 2
+print(ans)
