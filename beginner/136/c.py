09b2e0155d519c86866b5d3681d7f1331add9fd2
peacock <peacock0803sz@gmail.com>
2019-09-05 02:28:43 +0900

    :zap: Pass ABC136C

--- beginner/136/c.py
+++ beginner/136/c.py
@@ -1,15 +1,13 @@
 n = int(input())
 h = [int(i) for i in input().split()]
-check = []
+h[0] -= 1
 if len(h) == 1:
     print('Yes')
 else:
-    for i in range(2, n):
-        if h[i - 2] - h[i] >= 2:
-            check.append(False)
-        else:
-            check.append(True)
-    if len(set(check)) == 1:
-        print('Yes')
-    else:
-        print('No')
+    for i in range(1, n):
+        if h[i] > h[i - 1]:
+            h[i] -= 1
+        elif h[i] < h[i - 1]:
+            print('No')
+            exit()
+    print('Yes')
